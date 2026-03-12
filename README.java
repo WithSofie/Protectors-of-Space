90e0c6c449c960be1c355c26e116debdb1d59037
WithoutSophie <99111597+WithSofie@users.noreply.github.com>
2026-03-11 23:44:43 -0600

    Update README.java

--- README.java
+++ README.java
@@ -1,33 +1,21 @@
-
-
-
-
 /**
- * Copyright (c) CrispyFlowe 
- * DO NOT ALTER OR REMOVE THIS NOTICES
+ * Copyright (c) WithSofie
+ * DO NOT ALTER OR REMOVE THIS NOTIFICATION
  * 
  * ! THIS IS THE PREVIEW VERSION
  * 
- * 
- * Made By Cflowe
  * Published 2022 / 3 / 3
  * 
  * Subscribe my channel | contact me: 
- * Youtube - 1: https://www.youtube.com/channel/UCzD7B_hlD90RUaCyHZzBmWg
- * Youtube - 2: https://m.youtube.com/channel/UCMJBARUQArhtbNp7v13n1RQ
- * Github: https://github.com/CrispyFlowe
+ * Github: https://github.com/WithSofie
  * Gmail: (1) cflowe1357@gmail.com | (2) cflowetourist@gmail.com
  * 
  * --- --- --- --- ---
  * Please read main_file doc while reading source code. Do not public this source code
  * This is a original game made by {@code CrispyFlowe}.
- * You {@code Can't} public without permission.
+ * You {@code Can't} publish without permission.
  * 
- * 
- * @author CrispyFlowe: cflowe1357@gmail.com
- * @author CfloweTourist: cflowetourist@gmail.com
- * 
- * 
+ * @author WithSofie: cflowe1357@gmail.com
  */
 
 
